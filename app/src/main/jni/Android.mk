e2f27659230ad4fb4c1605489e68f02dec7f862c
abismbirk <abismbirk@github.com>
2026-05-07 10:46:32 +0300

    fix: remove broken import-module

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,6 +3,4 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ShadowLib
 LOCAL_SRC_FILES := native-lib.cpp
 LOCAL_LDLIBS := -llog
-LOCAL_C_INCLUDES := $(NDK_ROOT)/sources/cxx-stl/llvm-libc++/include
 include $(BUILD_SHARED_LIBRARY)
-$(call import-module, cxx-stl/llvm-libc++)
